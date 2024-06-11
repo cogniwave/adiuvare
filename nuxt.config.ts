b89159775bfbf37b71bbf47284134cf5ca1b756c
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2024-06-11 15:58:41 +0100

    update logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,16 +11,19 @@ export default defineNuxtConfig({
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       title: "Adiuvare",
-      meta: [{ name: "google-adsense-account", content: "ca-pub-1091633097511683" }],
+      meta: [
+        { name: "google-adsense-account", content: "ca-pub-1091633097511683" },
+        { name: "msapplication-TileColor", content: "#eceff1" },
+        { name: "theme-color", content: "#ECEFF1" },
+      ],
       link: [
-        {
-          rel: "stylesheet",
-          href: "https://use.fontawesome.com/releases/v5.0.13/css/brand.css",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://use.fontawesome.com/releases/v5.0.13/css/solid.css",
-        },
+        { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.0.13/css/brand.css" },
+        { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.0.13/css/solid.css" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
       ],
     },
   },
