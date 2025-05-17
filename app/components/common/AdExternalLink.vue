e1a7684712bed54b8e1ee0d758f2844199901d74
Tiago Ribeiro <tiago.ribeiro@cogniwave.pt>
2025-05-17 21:28:41 +0100

    UI: org detaisl

---
+++ app/components/common/AdExternalLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <a :href="href" target="blank" rel="noreferrer">
+    {{ label }}
+
+    <v-icon>fa-up-right-from-square</v-icon>
+  </a>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    href: { type: String, required: true },
+    label: { type: String, default: null },
+  });
+</script>
--- app/components/layout/AdMenu.vue
+++ app/components/layout/AdMenu.vue
@@ -8,22 +8,53 @@
 
     <v-list-item :title="$t('menu.home')" to="/" prepend-icon="fa-home" />
 
-    <v-list-item :title="$t('orgs.title')" to="/organizations" prepend-icon="fa-building" />
+    <v-list-item
+      :title="$t('orgs.title')"
+      :active="$route.path.includes('organizations')"
+      to="/organizations"
+      prepend-icon="fa-building"
+    />
 
     <v-divider class="my-1" />
 
     <template v-if="loggedIn">
-      <v-list-item :title="$t('nav.account')" to="/account" prepend-icon="fa-cog" />
+      <v-list-item
+        :title="$t('nav.account')"
+        :active="$route.path.includes('account')"
+        to="/account"
+        prepend-icon="fa-cog"
+      />
 
-      <v-list-item :title="$t('nav.profile')" to="/profile" prepend-icon="fa-user" />
+      <v-list-item
+        :title="$t('nav.profile')"
+        :active="$route.path.includes('profile')"
+        to="/profile"
+        prepend-icon="fa-user"
+      />
 
-      <v-list-item :title="$t('nav.logout')" to="/logout" prepend-icon="fa-right-from-bracket" @click="clear" />
+      <v-list-item
+        :title="$t('nav.logout')"
+        :active="$route.path.includes('logout')"
+        to="/logout"
+        prepend-icon="fa-right-from-bracket"
+        @click="clear"
+      />
     </template>
 
     <template v-else>
-      <v-list-item :title="$t('login.title')" to="/login" prepend-icon="fa-right-to-bracket" />
+      <v-list-item
+        :title="$t('login.title')"
+        :active="$route.path.includes('login')"
+        to="/login"
+        prepend-icon="fa-right-to-bracket"
+      />
 
-      <v-list-item :title="$t('register.title')" to="/register" prepend-icon="fa-user-plus" />
+      <v-list-item
+        :title="$t('register.title')"
+        :active="$route.path.includes('register')"
+        to="/register"
+        prepend-icon="fa-user-plus"
+      />
     </template>
   </v-list>
 </template>
